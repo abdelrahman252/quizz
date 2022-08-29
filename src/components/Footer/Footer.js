4a2df73ff94a1bdeaa12a79f2c18a8e124b66723
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2022-08-29 22:36:05 +0100

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://abdelrahaman-portfolio.netlify.app"
+        style={{ cursor: "pointer" }}
+      >
+        Abdelrahaman
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
